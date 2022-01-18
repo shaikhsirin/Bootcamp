a5fff88f2f0ef9e3ab01c4817806dc324196ae0c
shaikhsirin <shaikhsirin8080@gmail.com>
2022-01-18 15:16:55 +0530

    digit number write in word

---
+++ day5/selection_pp_if-elif-else/digit_to_weekday.sh
@@ -0,0 +1,27 @@
+# Read a single digit number and write in word
+
+#!/bin/bash
+read num
+
+if [ $num == 1 ];
+then
+	echo "Sunday";
+elif [ $num == 2 ];
+then
+	echo "Monday";
+elif [ $num == 3 ];
+then
+	echo "Tuesday";
+elif [ $num == 4 ];
+then
+	echo "Wednesday";
+elif [ $num == 5 ];
+then
+	echo "Thursday";
+elif [ $num == 6 ];
+then
+	echo "Friday";
+elif [ $num == 7 ];
+then
+	echo "Saturday";
+fi
